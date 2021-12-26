c2dab7c261ba3e884a15c773ca1e9a1df5b6fb7a
ashish4zemoso <ashish.dahiya@zemosolabs.com>
2021-12-27 05:19:27 +0530

    cleaned up listAllInitials function with arrow function and display method on listOfInitials

--- app.js
+++ app.js
@@ -15,15 +15,13 @@ const listOfNames = [
      lastName: 'Bahri'
     }
 ];
+
 const returnInitials = (firstName,lastName) => firstName.charAt(0)+lastName.charAt(0);
 
 function listAllInitials(returnInitials){
-    return listOfNames.map(function(obj){
-        return returnInitials(obj.firstName,obj.lastName);
-    });
+    return listOfNames.map(obj => returnInitials(obj.firstName,obj.lastName));
 }
+
 const listOfInitials = listAllInitials(returnInitials);
 
-listOfInitials.forEach(function(el){
-    console.log(el);
-});
+listOfInitials.forEach(el => console.log(el));
